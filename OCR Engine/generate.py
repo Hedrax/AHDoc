342d0163f6412135ad6942dcaf340c729672bf47
Ali ibrahim Ahmed <116113529+AliFeteha@users.noreply.github.com>
2024-08-01 11:39:34 +0300

    Update generate.py
    update it to have option to choose if it phase generator or word so it have concatination

--- OCR Engine/generate.py
+++ OCR Engine/generate.py
@@ -62,38 +62,36 @@ def draw_random_drops(image, num_drops, drop_radius, alpha):
     cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, image)
 
 
-def generate(cfg, type = 'train', input2= False, valid_aug=False):
+def generate(cfg, type='train', input2=False, valid_aug=False, get_only_specific_charachters=0):
+    if type == 'train':
+        data_dir = cfg.TRAIN_DATA_PATH
+    elif type == 'val':
+        data_dir = cfg.VALIDATION_DATA_PATH
+    else:
+        data_dir = cfg.TEST_DATA_PATH
 
-    match type:
-        case 'train':
-             data_dir = cfg.TRAIN_DATA_PATH
-        case 'val':
-             data_dir = cfg.VALIDATION_DATA_PATH
-        case _:
-            data_dir = cfg.TEST_DATA_PATH
-
-    #without the directory
     image_paths = []
     labels = []
 
     char_to_num = StringLookup(vocabulary=cfg.characters, mask_token=None)
-    
-    with open(osp.join(data_dir, 'labels.txt'), encoding="utf-8") as f:
-        #format :: 1_gtLabel
-        read = f.readlines()
 
-        for i in range (len(read)):
-            #find the _
+    with open(osp.join(data_dir, 'labels.txt'), encoding="utf-8") as f:
+        read = f.readlines()
+        for i in range(len(read)):
             split = 1
-            while(True):
-                if (read[i][split] == '_'):
-                    break;
-                split+=1
-            image_paths.append(str(read[i][0:split])+'.png')
-            labels.append(read[i][split+1:-1])
+            while True:
+                if read[i][split] == '_':
+                    break
+                split += 1
+            
+            if get_only_specific_charachters:
+                if get_only_specific_charachters != len(str(read[i][split + 1:-1])):
+                    continue
+
+            image_paths.append(str(read[i][0:split]) + '.png')
+            labels.append(read[i][split + 1:-1])
 
     dataset_size = len(image_paths)
-    #array of numbers from 0 to dataset_size
     indices = np.arange(dataset_size)
     np.random.shuffle(indices)
 
@@ -101,7 +99,7 @@ def generate(cfg, type = 'train', input2= False, valid_aug=False):
         batch_images = np.zeros([cfg.BATCH_SIZE, cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH, 3], dtype=np.float32)
         batch_labels = np.zeros((cfg.BATCH_SIZE, cfg.MAX_LEN), dtype=np.int32)
         return batch_images, batch_labels
-    
+
     current_idx = 0
     b = 0
 
@@ -112,111 +110,71 @@ def generate(cfg, type = 'train', input2= False, valid_aug=False):
         if b == 0:
             batch_images, batch_labels = init_input()
 
-        #choosing between 1 and max number -1 to be added
-        num_of_words = np.random.randint(1, cfg.MAX_NUMBER_OF_WORDS)
-        
-        #concate words of the upper defined indices and print out the true labels of them
-        #get the images of those indices and concatenate with each other along with labels 
-
-
-
-        # Read the images
-        image = cv2.imread(data_dir+image_paths[current_idx])
+        image = cv2.imread(osp.join(data_dir, image_paths[current_idx]))
         label = labels[current_idx]
 
-        #defining max height
         max_height = image.shape[0]
-        
-        random_idx = np.random.randint(0, dataset_size, size=num_of_words).tolist()
-        for i in range(1, num_of_words):
-            image2 = cv2.imread(data_dir+image_paths[random_idx[i]])
+        random_idx = np.random.randint(0, dataset_size, size=cfg.MAX_NUMBER_OF_WORDS).tolist()
 
-            #making sure both have same height
-            if (max_height > image2.shape[0]):
-                image2 = resize_image(image2, max_height, 0, True)
-            else:
-                max_height = image2.shape[0]
-                image = resize_image(image, max_height, 0, True)
+        if cfg.WORD == True:
+            for i in range(1, cfg.MAX_NUMBER_OF_WORDS):
+                image2 = cv2.imread(osp.join(data_dir, image_paths[random_idx[i]]))
 
-            #some white space
-            padding_width = 5 
-            white_space = np.ones((max_height, padding_width, 3), dtype=np.uint8) * 255
+                if max_height > image2.shape[0]:
+                    image2 = resize_image(image2, max_height, 0, True)
+                else:
+                    max_height = image2.shape[0]
+                    image = resize_image(image, max_height, 0, True)
 
-            # Concatenate the images horizontally
-            image = np.hstack((image2, white_space, image))
-            label += ' ' + labels[random_idx[i]]
-        
-        
-        image = resize_image(image, cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH)
+                padding_width = 5
+                white_space = np.ones((max_height, padding_width, 3), dtype=np.uint8) * 255
+                image = np.hstack((image2, white_space, image))
+                label += ' ' + labels[random_idx[i]]
+        elif cfg.WORD == False:
+            image = resize_image(image, cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH)
 
-        #30% augmentend data
-        aug_key = (np.random.randint(0, 101) > cfg.CLEAN_DATA_PERCENTAGE)
-
-        #Data Augmentation
-        if (valid_aug and aug_key):
+        if valid_aug:
             height, width = cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH
             random_list = np.random.randint(0, 2, size=5).tolist()
-            #add noise
-            if (random_list[0]):
+
+            if random_list[0]:
                 draw_random_drops(image, cfg.NUM_OF_DROPS, cfg.DROPS_RADIUS, cfg.DROPS_ALPHA)
-            
-            #draw shapes
-            if (random_list[1]):
-                polygon_points = generate_random_polygon(image.shape)
-                overlay = image.copy()
-                
-                color = cfg.POLYGON_COLOR  
-                alpha = cfg.POLYGON_ALPHA  # Transparency factor
-                
-                cv2.fillPoly(overlay, [polygon_points], color)
-                image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-            
-            #rotate with max 15 degrees
-            if (random_list[2]):
-                
+            if random_list[1]:
+                kernel_size = (3, 3)
+                iterations = 1
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                _, binary_image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+                kernel = np.ones(kernel_size, np.uint8)
+                dilated_image = cv2.dilate(binary_image, kernel, iterations=iterations)
+                bold_text_image = cv2.bitwise_not(dilated_image)
+                image = cv2.cvtColor(bold_text_image, cv2.COLOR_GRAY2BGR)
+            if random_list[2]:
                 angle = np.random.uniform(-cfg.MAX_ROTATION_ANGLE, cfg.MAX_ROTATION_ANGLE)
-                
-                # Calculate the center of the image
                 center = (width // 2, height // 2)
-                
-                # Get the rotation matrix
                 rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-                
-                # Apply the rotation
                 image = cv2.warpAffine(image, rotation_matrix, (width, height), borderMode=cv2.BORDER_REPLICATE)
-
-            
-            #blur
-            if (random_list[3]):  
+            if random_list[3]:
                 image = cv2.GaussianBlur(image, (5, 5), 0)
-
-                
-            
-            #dilute
-            if (random_list[3]):
+            if random_list[4]:
                 kernel = np.ones((cfg.DILUTE_KERNAL_FACTOR, cfg.DILUTE_KERNAL_FACTOR), np.uint8)
                 image = cv2.dilate(image, kernel, iterations=1)
-                
-        
 
-        #normalize image
-        image = tf.cast(image, np.float32) / 255.0
+        try:
+            image = tf.cast(image, np.float32) / 255.0
+            label = vectorize_label(label, char_to_num, cfg)
+            batch_images[b] = image
+            batch_labels[b] = label
+            b += 1
+        except:
+            pass
 
-
-        label = vectorize_label(label, char_to_num,cfg)
-
-
-        
-        batch_images[b] = image
-        batch_labels[b] = label
-        b += 1
         current_idx += 1
 
-
         if b == cfg.BATCH_SIZE:
-            if (input2):
+            b = 0
+            if input2:
                 yield [batch_images, batch_labels], batch_labels
             else:
                 yield batch_images, batch_labels
-            b = 0
+
 
